ee3297b531dbe7fcdab63a2fce4fcf21c4c60b60
bismapro <bisma.pro999@gmail.com>
2025-09-27 22:03:01 +0700

    feat: add dotenv, add icon launcher for android & ios, and update outdated package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
 import 'core/routes/app_routes.dart';
-
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+void main() async{
+  try {
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    // ignore: avoid_print
+    print('Error loading .env file: $e');
+    throw Exception('Failed to load .env file');
+  }
   runApp(const MyApp());
 }
 
